7acf330768bc637481580624b055ae63a5af8a6c
kyawsithu1345-hub <sithu.kyaw1345@gmail.com>
2026-03-24 12:25:16 +0630

    Add files via upload

--- array.js
+++ array.js
@@ -1,40 +1,52 @@
 /* array.js */
 let playlist = [
     { 
-        name: "Alan Walker x Benjamin Ingrosso - Man On The Moon", 
-        url: "https://jyhxqlwhvptzmvtzrytg.supabase.co/storage/v1/object/public/music/Alan%20Walker%20x%20Benjamin%20Ingrosso%20-%20Man%20On%20The%20Moon%20(Official%20Music%20Video).mp3" 
+    name: "Alan Walker x Benjamin Ingrosso - Man On The Moon", 
+    url: "https://jyhxqlwhvptzmvtzrytg.supabase.co/storage/v1/object/public/music/Alan%20Walker%20x%20Benjamin%20Ingrosso%20-%20Man%20On%20The%20Moon%20(Official%20Music%20Video).mp3" 
     },
     { 
-        name: "Alan Walker & Faouzia - Last Song", 
-        url: "https://jyhxqlwhvptzmvtzrytg.supabase.co/storage/v1/object/public/music/Alan%20Walker,%20Faouzia%20-%20Last%20Song%20(Official%20Lyric%20Video).mp3" 
+    name: "Alan Walker & Faouzia - Last Song", 
+    url: "https://jyhxqlwhvptzmvtzrytg.supabase.co/storage/v1/object/public/music/Alan%20Walker,%20Faouzia%20-%20Last%20Song%20(Official%20Lyric%20Video).mp3" 
     },
     { 
-        name: "Alan Walker ft Sorana - Mind of a Warrior", 
-        url: "https://jyhxqlwhvptzmvtzrytg.supabase.co/storage/v1/object/public/music/Alan%20Walker%20ft%20Sorana%20-%20Mind%20of%20a%20Warrior%20.mp3" 
-    },
-    { 
-        name: "Elara Vea - Whispers of the Sky", 
-        url: "https://jyhxqlwhvptzmvtzrytg.supabase.co/storage/v1/object/public/music/Elara%20Vea%20-%20Whispers%20of%20the%20Sky%20(Official%20Music%20Video).mp3" 
+    name: "Alan Walker ft Sorana - Mind of a Warrior", 
+    url: "https://jyhxqlwhvptzmvtzrytg.supabase.co/storage/v1/object/public/music/Alan%20Walker%20ft%20Sorana%20-%20Mind%20of%20a%20Warrior%20.mp3" 
     },
     {
-        name: "Halsey - Without Me",
-        url: "https://jyhxqlwhvptzmvtzrytg.supabase.co/storage/v1/object/public/music/Halsey%20-%20Without%20Me.mp3"
+    name: "Bella Poarch - Living Hell",
+    url: "https://files.catbox.moe/txa5zl.mp3"
     },
     { 
-        name: "NiExshadow - If I Live Without You", 
-        url: "https://jyhxqlwhvptzmvtzrytg.supabase.co/storage/v1/object/public/music/NiExshadow%20-%20If%20I%20Live%20Without%20You.mp3" 
+    name: "Elara Vea - Whispers of the Sky", 
+    url: "https://jyhxqlwhvptzmvtzrytg.supabase.co/storage/v1/object/public/music/Elara%20Vea%20-%20Whispers%20of%20the%20Sky%20(Official%20Music%20Video).mp3" 
+    },
+    {
+    name: "Halsey - Without Me",
+    url: "https://jyhxqlwhvptzmvtzrytg.supabase.co/storage/v1/object/public/music/Halsey%20-%20Without%20Me.mp3"
+    },
+    //Dropbox
+   {
+    name: "MIIA - Dynasty",
+    url: "https://www.dropbox.com/scl/fi/s6vo09xgk7bxl1mdmafaf/MIIA-Dynasty.mp3?rlkey=7wqe6w69jg1l6xid7l8n0j2cd&st=4v8i4irx&raw=1"
+   },
+   { 
+    name: "NiExshadow - If I Live Without You", 
+    url: "https://jyhxqlwhvptzmvtzrytg.supabase.co/storage/v1/object/public/music/NiExshadow%20-%20If%20I%20Live%20Without%20You.mp3" 
     },
     { 
-        name: "NiExshadow - You Will Never Stay Forever",
-        url: "https://jyhxqlwhvptzmvtzrytg.supabase.co/storage/v1/object/public/music/NiExshadow%20-%20You%20Will%20Never%20Stay%20Forever.mp3"
+    name: "NiExshadow - You Will Never Stay Forever",
+    url: "https://jyhxqlwhvptzmvtzrytg.supabase.co/storage/v1/object/public/music/NiExshadow%20-%20You%20Will%20Never%20Stay%20Forever.mp3"
     },
+    {
+    name: "Rihanna - Diamonds",
+    url: "https://www.dropbox.com/scl/fi/m2440idwvmkbleuwrnu7m/Rihanna-Diamonds.mp3?rlkey=veg3zsxyvpkcwfr61w7x863b9&st=ukbrn4ha&raw=1"},
     {
     //Catbox
-        name: "Tate McRae - Sports car",
-        url: "https://files.catbox.moe/23ciof.mp3"
+    name: "Tate McRae - Sports car",
+    url: "https://files.catbox.moe/23ciof.mp3"
     },
     {
-        name: "TYSM - Normal No More",
-        url: "https://files.catbox.moe/aautmt.mp3"
+    name: "TYSM - Normal No More",
+    url: "https://files.catbox.moe/aautmt.mp3"
     },
 ];
